5340948e325463b80842a8cad2051c2242a334b3
Saikat <saikatrock2k@gmail.com>
2026-04-14 21:34:00 +0530

    Problem of the day solved

---
+++ 14_4_26_gfg.py
@@ -0,0 +1,9 @@
+#Problem name: Remove Spaces
+'''
+Well no Intuation or approach for this one,
+python have a function which removes spaces from the string.
+'''
+
+class Solution:
+    def removeSpaces(self, s):
+        return s.replace(" ", "")
